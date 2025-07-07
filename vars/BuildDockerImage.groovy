ad4f19c3c65cee43e9dfbe5c2963d1c45e65aec2
buxiaomo <95112082@qq.com>
2025-07-07 17:31:22 +0800

    first commit

---
+++ vars/BuildDockerImage.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+
+/*
+// docker buildx --builder ${env.BUILDER} build --progress=plain --platform=${params.platform} -t ${env.REGISTRY_HOST}/${env.JOB_NAME}/dubhe-data-process:${BUILD_NUMBER} -t ${env.REGISTRY_HOST}/${env.JOB_NAME}/dubhe-data-process:latest --push .
+BuildDockerImage {
+    host = '192.168.1.1:5000'
+    project = 'projectName'
+    name = 'appname'
+    tag = '1.0.0'
+    platform = 'linux/amd64,linux/arm64'
+    path = './Dockerfile'
+}
+*/
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def host = config.get('host', env.REGISTRY_HOST)
+    def project = config.get('project', env.JOB_NAME)
+    def name = config.get('name', '')
+    def tag = config.get('tag', BUILD_NUMBER)
+    def platform = config.get('platform', 'linux/amd64')
+    def path = config.get('path', './Dockerfile')
+
+    sh "docker buildx --builder ${env.BUILDER} build --platform=${platform} -t ${host}/${project}/${name}:${tag} -t ${host}/${project}/${name}:latest --push . -f ${path}"
+}
+
+return this

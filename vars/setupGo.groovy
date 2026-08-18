c4ea9199710c99b416a23eae9676e5f9ef121002
buxiaomo <95112082@qq.com>
2026-08-19 00:28:21 +0800

    add setupgo

--- vars/setupGo.groovy
+++ vars/setupGo.groovy
@@ -28,7 +28,7 @@ def call(script, body) {
         }
         command << "tar -C /usr/local -xzf go${version}.tar.gz"
         command << "rm -f go${version}.tar.gz"
-        command << "✅ Golang ${version} 环境设置成功"
+        command << "echo \"✅ Golang ${version} 环境设置成功\""
         script.sh(label: 'Setup Go', script: command.join(" && "), returnStatus: true)
         return this
     } catch (Exception e) {

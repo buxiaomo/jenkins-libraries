c0248fee63a2b9df5ed1f788cb56dfaf9993f266
buxiaomo <95112082@qq.com>
2026-08-19 11:17:08 +0800

    fix

--- vars/setupGo.groovy
+++ vars/setupGo.groovy
@@ -1,11 +1,5 @@
 #!/usr/bin/env groovy
 
-/**
-* 设置 go 环境
-* @param script Jenkins脚本上下文
-* @param version golang版本
-*/
-
 def call(script, body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
@@ -16,34 +10,30 @@ def call(script, body) {
     def command = []
 
     try {
-        // 判断运行环境架构
         def arch = script.sh(script: "uname -m", returnStdout: true).trim()
         if (arch == "x86_64") {
             command << "wget https://go.dev/dl/go${version}.linux-amd64.tar.gz -O go${version}.tar.gz"
         } else if (arch == "aarch64") {
             command << "wget https://go.dev/dl/go${version}.linux-arm64.tar.gz -O go${version}.tar.gz"
         } else {
-            script.echo "❌ 不支持的架构: ${arch}"
-            return this
+            script.error("❌ 不支持的架构: ${arch}")
         }
+
+        command << "rm -rf /usr/local/go"
         command << "tar -C /usr/local -xzf go${version}.tar.gz"
         command << "rm -f go${version}.tar.gz"
+        command << "ln -sf /usr/local/go/bin/go /usr/local/bin/go"
+        command << "ln -sf /usr/local/go/bin/gofmt /usr/local/bin/gofmt"
         command << "echo \"✅ Golang ${version} 环境设置成功\""
         def status = script.sh(label: 'Setup Go', script: command.join(" && "), returnStatus: true)
         if (status != 0) {
             script.error("设置 Golang ${version} 环境失败，退出码：${status}")
         }
-
         script.env.GOROOT = "/usr/local/go"
-        script.env.PATH = "/usr/local/go/bin:${script.env.PATH}"
         return this
     } catch (Exception e) {
-        script.echo "❌ 设置 Golang ${version} 环境失败: ${e.getMessage()}"
-        return this
+        script.error("❌ 设置 Golang ${version} 环境失败: ${e.getMessage()}")
     }
 }
 
-
-
 return this
-

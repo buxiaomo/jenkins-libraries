5cb66bf151db9d2a3849692d7f80b151b6438b07
buxiaomo <95112082@qq.com>
2026-08-19 11:44:20 +0800

    fix

--- vars/setupAndroidSDK.groovy
+++ vars/setupAndroidSDK.groovy
@@ -28,15 +28,15 @@ def call(script, body) {
         command << "rm cmdline-tools.zip"
         command << "yes | /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --licenses"
         command << "/opt/android-sdk/cmdline-tools/latest/bin/sdkmanager \"platform-tools\" \"platforms;android-${compileSdk}\" \"build-tools;${compileSdk}.0.0\" \"ndk;${ndkVersion}\""
-        command << "echo \"✅ Android SDK ${version} 环境设置成功\""
+        command << "echo \"✅ Android SDK ${compileSdk} 环境设置成功\""
         def status = script.sh(label: 'Setup Android SDK', script: command.join(" && "), returnStatus: true)
         if (status != 0) {
-            script.error("设置 Android SDK ${version} 环境失败，退出码：${status}")
+            script.error("设置 Android SDK ${compileSdk} 环境失败，退出码：${status}")
         }
         script.env.ANDROID_HOME = ANDROID_HOME
         return this
     } catch (Exception e) {
-        script.echo "❌ 设置 Android SDK ${version} 环境失败: ${e.getMessage()}"
+        script.echo "❌ 设置 Android SDK ${compileSdk} 环境失败: ${e.getMessage()}"
         return this
     }
 }

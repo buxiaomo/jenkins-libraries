adb18105af8798535e3852dd824a5f5856f3284c
buxiaomo <95112082@qq.com>
2026-08-27 14:41:24 +0800

    update

--- vars/setupDocker.groovy
+++ vars/setupDocker.groovy
@@ -11,15 +11,14 @@ def call(script, body) {
     body()
 
     def version = config.version
-    def mirror = config.mirror
-    def command = []
-    if (mirror == null || mirror.trim() == "") {
-        command << "curl -fsSL https://get.docker.com | bash -s docker --version ${version} --no-autostart"
-    } else{
-        command << "curl -fsSL https://get.docker.com | bash -s docker --mirror ${mirror} --version ${version} --no-autostart"
-    }
-    command << "echo \"✅ Docker ${version} 环境设置成功\""
-    def status = script.sh(label: 'Setup Docker', script: command.join(" && "), returnStatus: true)
+    def command = """
+if [ ! -f /package/docker-${version}.tgz ]; then
+    wget -q https://download.docker.com/linux/static/stable/x86_64/docker-${version}.tgz -O /package/docker-${version}.tgz
+fi
+tar -xf /package/docker-${version}.tgz -C /usr/bin --strip-components=1
+docker -v
+"""
+    def status = script.sh(label: 'Setup Docker', script: command, returnStatus: true)
     if (status != 0) {
         script.error("设置 Docker ${version} 环境失败，退出码：${status}")
     }

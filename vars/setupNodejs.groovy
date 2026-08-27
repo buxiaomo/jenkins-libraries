edf85de5a991e1ea9503261deece71c17e688be5
buxiaomo <95112082@qq.com>
2026-08-27 14:10:06 +0800

    update

--- vars/setupNodejs.groovy
+++ vars/setupNodejs.groovy
@@ -11,35 +11,14 @@ def call(script, body) {
     body()
 
     def version = config.version
-    def command = []
-
-    def os = script.sh(
-        script: ". /etc/os-release && echo \$ID",
-        returnStdout: true
-    ).trim()
-
-    switch (os) {
-        case "ubuntu":
-        case "debian":
-            command << "apt-get update"
-            command << "apt-get install -y curl"
-            command << "curl -fsSL https://deb.nodesource.com/setup_${version}.x | bash -"
-            command << "apt-get install -y nodejs"
-            break
-        case "centos":
-        case "rhel":
-        case "rocky":
-            command << "yum install -y curl"
-            command << "curl -fsSL https://rpm.nodesource.com/setup_${version}.x | bash -"
-            break
-
-        default:
-            script.error("不支持的操作系统: ${os}")
-    }
-
-
-    command << "echo \"✅ Node.js ${version} 环境设置成功\""
-    script.sh(label: 'Setup Node.js', script: command.join(" && "), returnStatus: true)
+    def cmd = """
+if [ ! -f /package/node-v${version}-linux-x64.tar.xz ]; then
+    wget -q https://nodejs.org/dist/${version}/node-v${version}-linux-x64.tar.xz -O /package/node-v${version}-linux-x64.tar.xz
+fi
+tar -xf /package/node-v${version}-linux-x64.tar.xz -C /usr/local --strip-components=1
+node -v
+"""
+    script.sh(label: 'Setup Node.js', script: cmd, returnStatus: true)
 }
 
 return this

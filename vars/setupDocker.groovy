4e59de1a7b9893ecfa46a54d77ec4131a51f8d7f
buxiaomo <95112082@qq.com>
2026-08-27 15:08:24 +0800

    update

--- vars/setupDocker.groovy
+++ vars/setupDocker.groovy
@@ -11,12 +11,18 @@ def call(script, body) {
     body()
 
     def version = config.version
+    def composeVersion = config.composeVersion ?: '5.5.0'
     def command = """
 if [ ! -f /package/docker-${version}.tgz ]; then
     wget -q https://download.docker.com/linux/static/stable/x86_64/docker-${version}.tgz -O /package/docker-${version}.tgz
+    wget -q https://github.com/docker/compose/releases/download/v${composeVersion}/docker-compose-linux-x86_64-v${composeVersion} -O /package/docker-compose
 fi
+mkdir -p /usr/local/lib/docker/cli-plugins
+cp /package/docker-compose /usr/local/lib/docker/cli-plugins
+chmox +x /usr/local/lib/docker/cli-plugins/docker-compose
 tar -xf /package/docker-${version}.tgz -C /usr/bin --strip-components=1
 docker -v
+docker compose version
 """
     def status = script.sh(label: 'Setup Docker', script: command, returnStatus: true)
     if (status != 0) {
